86d4155b4730cf9257e6f3d552a1bf6284660cc8
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-12 22:57:38 -0400

    added CLI, and stsrted it

--- lib/players.rb
+++ lib/players.rb
@@ -2,7 +2,8 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-require "players/version"
+require_relative './players/version'
+require_relative 'players/cli'
 
 module Players
   class Error < StandardError; end
---
+++ lib/players/cli.rb
@@ -0,0 +1,9 @@
+class CLI
+
+def run
+	puts "made a new cli and ran it"
+	Scrape.get_initial_data
+end
+
+
+end
